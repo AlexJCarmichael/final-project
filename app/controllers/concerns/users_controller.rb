95fecaac129905d4b751a762b422e87e336adf7f
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-02 14:47:33 -0500

    Modified user params and put flash messaging up for errors in sign up.

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < Clearance::UsersController
 
   def new
-    @user = user_from_params
+    @user = User.new
     render template: "users/new"
   end
 
   def create
     @user = user_from_params
-    if @user.password == user.password_confirmation
+    if @user.password == @user.password_confirmation
       if @user.save
         sign_in @user
         redirect_back_or url_after_create
       else
+        flash[:alert] = 'Username, email, password, name, and confirm password required.'
         render template: "users/new"
       end
     else
+      flash[:alert] = 'Passwords do not match'
       render template: "users/new"
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :name, :phone_number, :profile_pic)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :name, :phone_number, :profile_pic)
   end
 end
