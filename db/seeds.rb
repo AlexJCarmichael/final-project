db06c3f2e0ff8516abd73052078c450d316ee70e
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-03 09:03:15 -0500

    Adding depth to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.create(user_name: "Jaxom555", name: "Dane", email: "test@test.test", password: "password")
-
+user = User.create( user_name: "Jaxom555", name: "Dane", email: "test@test.test", password: "password" )
+user2 = User.create( user_name: "Vpatel90", name: "Vivek", email: "vivek@test.test", password: "password" )
+user3 = User.create( user_name: "Vanderson", name: "Van", email: "van@test.test", password: "password" )
 sheet = SheetTemplate.create( game_name: "Danes and Dragons" )
 
 jaxom = Character.create( name: "Jaxom Vanee", bio: "You know Jaxom", user_id: user.id, sheet_template_id: sheet.id )
+a_random_hero = Character.create( name: "A Random Hero", bio: "You know A Random Hero", user_id: user2.id, sheet_template_id: sheet.id )
+morales = Character.create( name: "Morales", bio: "You know Morales", user_id: user3.id, sheet_template_id: sheet.id )
 
-stat = Stat.create( name: "Strength", value: 6 )
-stat2 = Stat.create( name: "Dexterity", value: 12 )
+characters = [jaxom, a_random_hero, morales]
 
-CharStat.create( character_id: jaxom.id, stat_id: stat.id )
-CharStat.create( character_id: jaxom.id, stat_id: stat2.id )
+str1 = Stat.create( name: "Strength", value: 6 )
+str2 = Stat.create( name: "Strength", value: 3 )
+str3 = Stat.create( name: "Strength", value: 12 )
+str4 = Stat.create( name: "Strength", value: 9 )
+dex1 = Stat.create( name: "Dexterity", value: 1 )
+dex2 = Stat.create( name: "Dexterity", value: 12 )
+dex3 = Stat.create( name: "Dexterity", value: 15 )
+dex4 = Stat.create( name: "Dexterity", value: 18 )
 
-skill1 = Skill.create( name: "Acrobatics", value: 12 )
-skill2 = Skill.create( name: "Sword", value: 2 )
+strength = [str1, str2, str3, str4]
+dexterity = [dex1, dex2, dex3, dex4]
 
-CharSkill.create( character_id: jaxom.id, skill_id: skill1.id )
-CharSkill.create( character_id: jaxom.id, skill_id: skill2.id )
+acro1 = Skill.create( name: "Acrobatics", value: 12 )
+acro2 = Skill.create( name: "Acrobatics", value: 18 )
+acro3 = Skill.create( name: "Acrobatics", value: 6 )
+acro4 = Skill.create( name: "Acrobatics", value: 7 )
+sword1 = Skill.create( name: "Sword", value: 2 )
+sword2 = Skill.create( name: "Sword", value: 5 )
+sword3 = Skill.create( name: "Sword", value: 7 )
+sword4 = Skill.create( name: "Sword", value: 8 )
 
-equip1 = Equipment.create( name: "Katana", damage: "2d6", category: "Weapon", weight: "3lbs" )
-equip2 = Equipment.create( name: "Hermes Boots", category: "Armor", weight: "1lb", effects: "Allows the character to jump 30 feet." )
+acroArr = [acro1, acro2, acro3, acro4]
+swordArr = [sword1, sword2, sword3, sword4]
 
-CharEquip.create( character_id: jaxom.id, equipment_id: equip1.id )
-CharEquip.create( character_id: jaxom.id, equipment_id: equip2.id )
+weapon1 = Equipment.create( name: "Katana", damage: "2d6", category: "Weapon", weight: "3lbs" )
+weapon2 = Equipment.create( name: "Rapier", damage: "1d6+2", category: "Weapon", weight: "2lbs" )
+weapon3 = Equipment.create( name: "Broadsword", damage: "2d6+1", category: "Weapon", weight: "5lbs" )
+weapon4 = Equipment.create( name: "Dirk", damage: "1d6", category: "Weapon", weight: "1lbs" )
+armor1 = Equipment.create( name: "Hermes Boots", category: "Armor", weight: "1lb", effects: "Allows the character to jump 30 feet." )
+armor2 = Equipment.create( name: "Inari Okami Robes", category: "Armor", weight: "2lb", effects: "Allows the character increased luck(GM's discretion)." )
+armor3 = Equipment.create( name: "Odin's Ring", category: "Armor", weight: ".5lb", effects: "Grants the character +2 intelligence." )
+armor4 = Equipment.create( name: "Quetzalcoatl's bracer", category: "Armor", weight: "1lb", effects: "Allows the character to deflect arrows." )
+
+weaponArr = [weapon1, weapon2, weapon3, weapon4]
+armorArr = [armor1,armor2, armor3, armor4]
+
+characters.each do |char|
+  CharStat.create( character_id: char.id, stat_id: strength.sample.id )
+  CharStat.create( character_id: char.id, stat_id: dexterity.sample.id )
+
+  CharSkill.create( character_id: char.id, skill_id: acroArr.sample.id )
+  CharSkill.create( character_id: char.id, skill_id: swordArr.sample.id )
+
+  CharEquip.create( character_id: char.id, equipment_id: weaponArr.sample.id )
+  CharEquip.create( character_id: char.id, equipment_id: armorArr.sample.id )
