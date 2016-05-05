54d5302b89e222756d54266498e97be2acfaf44a
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-05 10:52:20 -0500

    Shows every aspect of the sheet_template on the show page

--- app/models/sheet_template.rb
+++ app/models/sheet_template.rb
@@ -2,5 +2,11 @@ class SheetTemplate < ActiveRecord::Base
   has_many :game_sheets
   has_many :characters
 
+  has_one :sheet_stat
+  has_many :stats, through: :sheet_stat
+
+  has_one :sheet_skill
+  has_many :skills, through: :sheet_skill
+
   validates :game_name, presence: true
 end
