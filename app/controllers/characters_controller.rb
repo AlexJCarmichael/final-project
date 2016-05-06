e55dd5c69b333eca1e79eed217feb0947152b92c
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-06 13:19:32 -0500

    Able to create a character from a sheet_template

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,8 +4,8 @@ class CharactersController < ApplicationController
   end
 
   def new
-    @player = current_user.players.first
     @game = GameSession.find(params.fetch(:session_id))
+    @player = current_user.players.where(game_session_id: @game.id)
     @character = Character.new
   end
 
@@ -14,9 +14,11 @@ class CharactersController < ApplicationController
   end
 
   def create
-    @character = current_user.characters.build(char_params)
-      if @character.save!
-        redirect_to edit_character_path(@character)
+    # character = current_user.characters.build(char_params)
+    character = current_user.characters.build(char_params)
+      if character.save!
+        assign_defaults(character)
+        redirect_to character_path(character)
       else
         flash[:alert] = "Character could not be created"
       end
@@ -29,5 +31,17 @@ class CharactersController < ApplicationController
 
   def char_params
     params.require(:character).permit(:name, :sheet_template_id, :bio, :player_id)
+  end
+
+  def assign_defaults(character)
+    sheet = SheetTemplate.find(character.sheet_template_id)
+
+    char_skills = sheet.skills.map { |skl| Skill.create(name: skl.name) }
+    char_skills.map do |skill|
+      CharSkill.create(skill_id: skill.id, character_id: character.id)
+    end
+
+    char_stats = sheet.stats.map { |st| Stat.create( name: st.name )}
+    char_stats.map { |stat| CharStat.create( stat_id: stat.id, character_id: character.id ) }
   end
 end
