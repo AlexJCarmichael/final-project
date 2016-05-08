20892243fbff6bcb5071eab8b822aa2672ec9f3b
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-08 14:12:26 -0500

    Able to make a new sheet template for a game.

---
+++ app/assets/javascripts/components/assign_sheet.js.jsx
@@ -0,0 +1,59 @@
+var AssignSheet = React.createClass ({
+  getInitialState: function (){
+    return {
+      postText: '',
+      divClass: "hide"
+    };
+  },
+
+  handleClick1: function (){
+    if (this.state.divClass === "hide") {
+      this.setState({divClass: ""});
+    }else {
+      this.setState({divClass: "hide"});
+    }
+  },
+
+  handleChange: function (event) {
+      this.setState({
+        postText: event.target.value
+    });
+  },
+
+  handleDown: function (event) {
+    if (event.keyCode === 13) {
+      this.handlePost();
+    }
+  },
+
+  handlePost: function () {
+    $.ajax({
+      method: "POST",
+      url: "/sheet_templates/",
+      data: {
+        sheet_template: {
+          game_name: this.state.postText
+        }
+      },
+      success: function(response) {
+        window.location.replace("/sheet_templates/" + response.redirect_id);
+      }
+    })
+  },
+
+  render: function () {
+    return (
+    <div>
+      <p onClick={this.handleClick1} className="clicky-button"><strong>Do you want to create your own sheet for this game?</strong></p>
+      <div className={this.state.divClass}>
+        <input className="center"
+          placeholder="Name the Sheet Template"
+          value={this.state.postText}
+          onKeyDown={this.handleDown}
+          onChange={this.handleChange}/>
+      </div>
+      <p onClick={this.handleClick2} className="clicky-button"><strong>Do you want to use a sheet that already exists?</strong></p>
+    </div>
+    );
+  }
+});
--- app/controllers/sheet_templates_controller.rb
+++ app/controllers/sheet_templates_controller.rb
@@ -3,6 +3,15 @@ class SheetTemplatesController < ApplicationController
     @sheet = SheetTemplate.find(params.fetch(:id))
   end
 
+  def new
+    @game_id = params.fetch(:game)
+  end
+
+  def create
+    sheet = SheetTemplate.create(sheet_params)
+    render json: { message: "Sheet created!", redirect_id: sheet.id }
+  end
+
   def grabber
     sheet = SheetTemplate.find(params.fetch(:id))
     @skills = sheet.skills
@@ -11,5 +20,10 @@ class SheetTemplatesController < ApplicationController
       skills: @skills,
       stats: @stats
       }
+  end
+
+  private
+  def sheet_params
+    params.require(:sheet_template).permit(:game_name)
   end
 end
