3d21118499049652ad4c454cd10d018a0c05275f
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-11 14:10:01 -0500

    Able to return a random number of die sides a random number of rolls back to the chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,6 +27,9 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
+    if message.body[0..4] == "/roll"
+      message.roll_dice
+    end
     if message.save
       respond_to do |format|
         format.json do
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,21 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :chat_session
 
+  def roll_dice
+    if self.body.match(/\d{1,3}d\d{1,3}/)
+      num_times = self.body.match(/\b\d{1,3}/).to_s
+      num_times = num_times.to_i
+      num_of_sides = self.body.match(/\d{1,3}\b/).to_s
+      num_of_sides = num_of_sides.to_i
+      rolls = num_times.times.collect { return_die_result(num_of_sides) }
+      self.body = rolls.join(" ")
+    end
+  end
+
+  def return_die_result(sides)
+    rand(1..sides)
+  end
+
   def as_json(_ = nil)
     super(include: [:user])
   end
