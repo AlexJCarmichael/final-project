50855243f0931d7aa796e9eb4758c059fa2b6b09
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-11 15:07:36 -0500

    Switching to secure random

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,12 @@
+require 'SecureRandom'
 class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :chat_session
 
   def roll_dice
     if self.body.match(/\d{1,3}d\d{1,3}/)
-      num_times = self.body.match(/\b\d{1,3}/).to_s
-      num_times = num_times.to_i
-      num_of_sides = self.body.match(/\d{1,3}\b/).to_s
-      num_of_sides = num_of_sides.to_i
+      num_times = self.body.match(/\b\d{1,3}/).to_s.to_i
+      num_of_sides = self.body.match(/\d{1,3}\b/).to_s.to_i
       original = self.body.match(/\d{1,3}d\d{1,3}/).to_s
       rolls = num_times.times.collect { return_die_result(num_of_sides) }
       self.body = "rolls #{original}, resulting in #{rolls.join(", ")} for a total of #{rolls.reduce(:+)}"
@@ -18,7 +17,7 @@ class Message < ActiveRecord::Base
   end
 
   def return_die_result(sides)
-    rand(1..sides)
+    SecureRandom.random_number(sides-1) + 1
   end
 
   def as_json(_ = nil)
