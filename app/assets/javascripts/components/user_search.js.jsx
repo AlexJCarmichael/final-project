317180ed2d1bc71fd50a9db3b38fbc966e10c675
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-12 15:21:29 -0500

    Changed some of the dashboard styling on th main page. Search bar is no longer hidden for friends.

--- app/assets/javascripts/components/user_search.js.jsx
+++ app/assets/javascripts/components/user_search.js.jsx
@@ -4,7 +4,7 @@ var UserSearch = React.createClass({
         allUsers: [],
         filteredUsers: [],
         search: "",
-        divClass: "hide"
+        divClass: ""
       };
     },
 
@@ -58,10 +58,18 @@ var UserSearch = React.createClass({
       }
     },
 
+    displayBreak: function() {
+      if (this.state.search.length > 1) {
+        return (<h6 className="center">Click to make a friend request</h6>);
+      } else {
+        return (<span></span>);
+      }
+    },
+
     render: function() {
       return (
         <div>
-          <h6 onClick={this.handleClick} className={this.props.buttonStyle}>New friend</h6>
+          <h6 onClick={this.handleClick} className={this.props.buttonStyle}>Request a friend</h6>
           <div className={this.state.divClass}>
             <input type="text"
             onChange={this.filterList} value={this.state.search}
@@ -70,6 +78,7 @@ var UserSearch = React.createClass({
           <div>
             {this.userNames()}
           </div>
+            {this.displayBreak()}
         </div>
       );
     },
