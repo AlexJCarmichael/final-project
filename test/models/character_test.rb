1db68cb8d2028e6fcaccebd4a3aed9beac2ed445
Dane Carmichael <carmichaeldane@gmail.com>
2016-05-30 15:41:57 -0500

    CLeaning up fixture.yml files where the database changed after creation. Wrote first test for character creation.

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @character = Character.new(name: "Reginaldson", sheet_template_id: 1, bio: "You know Reginaldson", player_id: 1)
+  end
+
+  test "should be valid" do
+    assert @character.valid?
+  end
 end
