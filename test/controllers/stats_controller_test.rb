0b4dc2e0824e1d7d80247d8d92dab7c249d403bd
Dane Carmichael <carmichaeldane@gmail.com>
2016-06-02 11:32:17 -0500

    Fixing a controller test, adding unit test command to travis.yml file

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StatsControllerTest < ActionController::TestCase
   def setup
-    @stat = stat.create(name: "Cool Stat")
+    @stat = Stat.create(name: "Cool Stat")
     @params =  {"stats"=>{"rank"=>"10"}, "id"=>@stat.id}
   end
 
   def test_find_update
-    stat = Stat.find(@params.fetch(:id))
+    stat = Stat.find(@params.fetch("id"))
     assert_equal(stat.id , @stat.id)
   end
 end
