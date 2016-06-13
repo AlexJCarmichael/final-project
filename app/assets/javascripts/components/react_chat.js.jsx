523adb76e1abf1bdb27072e66c84010263c2c425
Dane Carmichael <carmichaeldane@gmail.com>
2016-06-13 10:22:06 -0500

    Configuring the React Chat component to better conform to JS standards.

--- app/assets/javascripts/components/react_chat.js.jsx
+++ app/assets/javascripts/components/react_chat.js.jsx
@@ -1,46 +1,37 @@
+/* globals $, React */
+/* exported ReactChat */
 var ReactChat = React.createClass({
-  getInitialState: function () {
-    return {
-      messages: []
-    };
-  },
+    getInitialState: function() {
+        return {messages: []};
+    },
 
-  tick: function() {
-    var that = this;
-    var url = "/messages/?chatable_id=" + this.props.chatId + "&chatable_type=" + this.props.chatType;
-    var chatWindow = document.getElementById("chat-window");
-    var isScrolledToBottom = chatWindow.scrollHeight - chatWindow.clientHeight <= chatWindow.scrollTop + 5;
-    $.getJSON(url, function(response){
-      that.setState({
-        messages: response
-      })
-      if(isScrolledToBottom)
-        chatWindow.scrollTop = chatWindow.scrollHeight - chatWindow.clientHeight;
-    });
-  },
+    tick: function() {
+        var that = this;
+        var url = "/messages/?chatable_id=" + this.props.chatId + "&chatable_type=" + this.props.chatType;
+        var chatWindow = document.getElementById("chat-window");
+        var isScrolledToBottom = chatWindow.scrollHeight - chatWindow.clientHeight <= chatWindow.scrollTop + 5;
+        $.getJSON(url, function(response) {
+            that.setState({messages: response});
+            if (isScrolledToBottom)
+                chatWindow.scrollTop = chatWindow.scrollHeight - chatWindow.clientHeight;
+        });
+    },
 
-  componentDidMount: function() {
-    this.interval = setInterval(this.tick, 1000);
-  },
+    componentDidMount: function() {
+        this.interval = setInterval(this.tick, 1000);
+    },
 
-  componentWillUnmount: function() {
-    clearInterval(this.interval);
-  },
+    componentWillUnmount: function() {
+        clearInterval(this.interval);
+    },
 
-  render: function() {
-    return (
-      <div id="chat-window">
-        {this.state.messages.map(function(message){
-          return (
-            <MessageDisplay
-              key={message.id}
-              image={message.users_image}
-              name={message.users_name}
-              body={message.body}
-              />
-          );
-        })}
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div id="chat-window">
+                {this.state.messages.map(function(message) {
+                    return (<MessageDisplay key={message.id} image={message.users_image} name={message.users_name} body={message.body}/>);
+                })}
+            </div>
+        );
+    }
 });
--- app/models/user_chat.rb
+++ app/models/user_chat.rb
@@ -3,4 +3,12 @@ class UserChat < ActiveRecord::Base
   belongs_to :recipient, :class_name => "User"
 
   has_many :messages, as: :chatable
+
+  def display_name(current)
+    if self.recipient.name == current.name
+      self.user.name
+    else
+      self.recipient.name
+    end
+  end
 end
